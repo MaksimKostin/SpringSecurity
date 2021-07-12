8b0444ede2f040b8ff7f4b0bcf82391dc0834e7f
Maks <off_white_off@mail.ru>
2021-07-12 16:18:42 +0300

    Added "create" and "delete" method

--- src/main/java/com/spring/springsecurity/controller/DeveloperRestController.java
+++ src/main/java/com/spring/springsecurity/controller/DeveloperRestController.java
@@ -1,10 +1,7 @@
 package com.spring.springsecurity.controller;
 
 import com.spring.springsecurity.model.Developer;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -30,5 +27,17 @@ public class DeveloperRestController {
                 .findFirst()
                 .orElse(null);
     }
+
+    @PostMapping
+    public Developer create(@RequestBody Developer developer){
+        this.DEVELOPERS.add(developer);
+        return developer;
+    }
+
+    @DeleteMapping("{id}")
+    public void deleteById(@PathVariable Long id){
+        this.DEVELOPERS.removeIf(developer -> developer.getId().equals(id));
+    }
+
 
 }
