ff768114c79d7cdc023d750cb40836709865585a
Maks <off_white_off@mail.ru>
2021-07-12 17:06:01 +0300

    Added role authorization

--- src/main/java/com/spring/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/spring/springsecurity/config/SecurityConfig.java
@@ -1,7 +1,9 @@
 package com.spring.springsecurity.config;
 
+import com.spring.springsecurity.model.Role;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -16,7 +18,16 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+        http    .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers(HttpMethod.GET,"/api/**").hasAnyRole(Role.ADMIN.name(), Role.USER.name())
+                .antMatchers(HttpMethod.POST,"/api/**").hasRole(Role.ADMIN.name())
+                .antMatchers(HttpMethod.DELETE,"/api/**").hasRole(Role.ADMIN.name())
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
     }
 
     @Bean
